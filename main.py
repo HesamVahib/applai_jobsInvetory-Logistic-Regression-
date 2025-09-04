df5aef1712a6c87d49260eb7ce5a80783d7d24b3
Hesam V. <hesam.vahib@gmail.com>
2025-09-04 22:04:42 +0300

    fixing a small typo in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from jobs.jobs_ready_to_db import jobs_ready_to_db
 from telegram import update_message
 
 try:
-    jobs_ready_to_db()i
-    update_message("jobs suseccfully updated!")
+    jobs_ready_to_db()
+    update_message("jobs successfully updated!")
 except Exception as e:
     update_message(e)
